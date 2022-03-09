996efed86779b264e0c6b5dea03f420002426e27
BeatrizAntunes <>
2022-03-09 19:03:27 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+class Retangulo{
+ constructor(altura, largura){
+   this.altura = altura
+   this.largura = largura 
+  }
+
+calcularArea(){
+  return this.altura * this.largura;
+}
+}
+
+
+
+let retangulo = new Retangulo("10","10")
+let retangule = new Retangulo("30","60")
+let retangluo = new Retangulo("20", "40")
+
+console.log(retangulo.altura)
+console.log(retangulo.largura)
+console.log(retangule.altura)
+console.log(retangule.largura)
+console.log(retangluo.calcularArea())
+console.log(retangulo.calcularArea())
+console.log(retangule.calcularArea())
